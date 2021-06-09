de40f238497927f29d9e03373d8962043d05969b
ShantikalaGhale <50352797+ShantikalaGhale@users.noreply.github.com>
2021-06-09 22:06:26 +0545

    Update kanchan.php

--- kanchan.php
+++ kanchan.php
@@ -52,11 +52,14 @@
 <div class="form-check-inline">
     	<label>Gender: </label>
     </div>
-    	<div class="form-check-inline">
-      <label class="form-check-label" for="radio2">Male
-        <input type="radio" class="form-check-input" name="optradio" value="option2">
-      </label>
-    </div>
+    	
+	    <div class="form-check-inline">↩
+↩
+<input type="radio" id="form-check-input" name="optradio" value="option2">↩
+<label for="form-check-input">Male</label>↩
+↩
+    </div>↩
+	    
     <div class="form-check-inline">
       <label class="form-check-label" for="radio2">
         <input type="radio" class="form-check-input" name="optradio" value="option2">Female
