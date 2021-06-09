9902ac1323cd2cc0524857a75c35dd4e12eb72c3
ShantikalaGhale <50352797+ShantikalaGhale@users.noreply.github.com>
2021-06-09 21:40:33 +0545

    Update kanchan.php

--- kanchan.php
+++ kanchan.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>Register</title>
 </head>
